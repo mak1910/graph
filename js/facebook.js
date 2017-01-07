96b24f920674acf57a25e8173f7542282afd8928
Mridul Kothari <mak1910@outlook.com>
2017-01-07 21:59:00 +0530

    Converted tabs to spaces

--- js/facebook.js
+++ js/facebook.js
@@ -1,64 +1,64 @@
-var globalData 	= {};
-var facebook  	= {};
+var globalData  = {};
+var facebook    = {};
 
 facebook.init = function() {
-	var self = this;
-	globalData.accessToken = document.getElementById('AccessTokenInput').value;
-	async.waterfall([
-		function(cb) {
-			
-		}
-	], function(err) {
+    var self = this;
+    globalData.accessToken = document.getElementById('AccessTokenInput').value;
+    async.waterfall([
+        function(cb) {
+            
+        }
+    ], function(err) {
 
-	})
+    })
 }
 
 facebook.getProfile = function (accessToken, cb) {
-	var url = "https://graph.facebook.com/v2.8/me?fields=email%2Cname%2cid&access_token=" + accessToken;
-	$.get(url, function(result) {
-		cb(result);
-	});
+    var url = "https://graph.facebook.com/v2.8/me?fields=email%2Cname%2cid&access_token=" + accessToken;
+    $.get(url, function(result) {
+        cb(result);
+    });
 }
 
 facebook.nextHandeler = function (url, cb) {
-	$.get(url, function(result) {
-		if(result && result.paging && result.paging.next && result.data.length != 0) {
-			nextHandeler(result.paging.next, function(data) {
-				cb(result.data.concat(data));
-			});
-		} else {
-			cb(result.data);
-		}
-	}).fail(function() {
-		cb([]);
-	});
+    $.get(url, function(result) {
+        if(result && result.paging && result.paging.next && result.data.length != 0) {
+            nextHandeler(result.paging.next, function(data) {
+                cb(result.data.concat(data));
+            });
+        } else {
+            cb(result.data);
+        }
+    }).fail(function() {
+        cb([]);
+    });
 }
 
 facebook.getPosts = function (accessToken, userId, cb) {
-	var url = "https://graph.facebook.com/v2.8/" + userId + '/posts?access_token=' + accessToken;
-	nextHandeler(url, cb);
+    var url = "https://graph.facebook.com/v2.8/" + userId + '/posts?access_token=' + accessToken;
+    nextHandeler(url, cb);
 }
 
 facebook.getLikes = function (accessToken, postId, cb) {
-	var url = "https://graph.facebook.com/v2.8/" + postId + "/likes?access_token=" + accessToken;
-	nextHandeler(url, cb);
+    var url = "https://graph.facebook.com/v2.8/" + postId + "/likes?access_token=" + accessToken;
+    nextHandeler(url, cb);
 }
 
 facebook.getAllPostLikes = function (accessToken, posts, cb) {
-	var tasks = [];
-	_.each(posts, function(post) {
-		if(post && post.id) {
-			var task = (function(post) {
-				return function(callback) {
-					getLikes(accessToken, post.id, function(data) {
-						callback(null, data);
-					});
-				}
-			})(post);
-			tasks.push(task);
-		}
-	});
-	async.parallel(tasks, function(err, likes) {
-		cb(likes);
-	})
+    var tasks = [];
+    _.each(posts, function(post) {
+        if(post && post.id) {
+            var task = (function(post) {
+                return function(callback) {
+                    getLikes(accessToken, post.id, function(data) {
+                        callback(null, data);
+                    });
+                }
+            })(post);
+            tasks.push(task);
+        }
+    });
+    async.parallel(tasks, function(err, likes) {
+        cb(likes);
+    })
 }
